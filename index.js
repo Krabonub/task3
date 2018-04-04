9625c60dad18c562ae152ad999768dbad0e7e7a0
Krabonub <skok.itcraft@gmail.com>
2018-04-04 13:48:28 -0700

    complete task

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 (function(){
     var addCollectionCollectionNameInput = document.getElementById('add-collection-collectionName-input');
     var addPropertyCollectionNameInput = document.getElementById('add-property-collectionName-input');
-    var addPropertyPropertyNameInput = document.getElementById('add-property-propertyName-input');
+    var addPropertyPropertyObjectInput = document.getElementById('add-property-propertyName-input');
     var removeFirstPropertyCollectionNameInput = document.getElementById('remove-firstProperty-collectionName-input');
     var removeLastPropertyCollectionNameInput = document.getElementById('remove-lastProperty-collectionName-input');
     var clearAllCollectionNameInput = document.getElementById('clearAll-collectionName-input');
     var addCollectionButton = document.getElementById('add-collection-button');
     var addPropertyButton = document.getElementById('add-property-button');
-    var addPropertyButton = document.getElementById('remove-firstProperty-button');
-    var addPropertyButton = document.getElementById('remove-lastProperty-button');
-    var addPropertyButton = document.getElementById('clearAll-button');
+    var removeFirstPropertyButton = document.getElementById('remove-firstProperty-button');
+    var removeLastPropertyButton = document.getElementById('remove-lastProperty-button');
+    var clearAllButton = document.getElementById('clearAll-button');
     var resultParagraph = document.getElementById('result-paragraph');
     
     var collections = {};
+    function printCollections(){
+        resultParagraph.innerHTML = '';
+        for(var i in collections){
+            var tmpP = document.createElement('p');
+            tmpP.innerHTML=`<strong>${i}</strong>`;
+            for(var j in collections[i].properties){
+                var tmpDiv = document.createElement('div');
+                tmpDiv.innerHTML = `<span>${collections[i].properties[j].name}</span>: <span>${collections[i].properties[j].value}</span><br>`;
+                tmpP.appendChild(tmpDiv);
+            }
+            resultParagraph.appendChild(tmpP);
+        }
+    }
     function createCollection(name){
         var obj = {
             name : name,
@@ -35,7 +48,6 @@
             }
         };
         collections[name] = obj;
-        console.log(collections);
         return obj;
     }
     var collection1 = createCollection("COLLECTION1");
@@ -45,15 +57,37 @@
     collection2.clearAll();
     /////////////////////
     addCollectionButton.onclick = function(){
-        var tmpCollection = createCollection(addCollectionCollectionNameInput.value);
-        var tmpElement = document.createElement('p');
-        tmpElement.innerHTML=`<p>
-        <strong>${tmpCollection.name}</strong>
-        </p>`;
-        resultParagraph.appendChild(tmpElement);
+        createCollection(addCollectionCollectionNameInput.value);
+        printCollections();
         addCollectionCollectionNameInput.value = '';
     }
     addPropertyButton.onclick = function(){
-
+        var match = addPropertyPropertyObjectInput.value.match(/\w+/g);
+        //console.log(match);
+        console.log(addPropertyCollectionNameInput.value);
+        collections[addPropertyCollectionNameInput.value].addValue({
+            [match[0]] : match[1],
+            [match[2]] : match[3]
+        });
+        printCollections();
+        addPropertyCollectionNameInput.value = '';
     }
+    removeFirstPropertyButton.onclick = function(){
+        collections[removeFirstPropertyCollectionNameInput.value].removeFirst();
+        printCollections();
+        removeFirstPropertyCollectionNameInput.value = '';
+    }
+    removeLastPropertyButton.onclick = function(){
+        collections[removeLastPropertyCollectionNameInput.value].removeLast();
+        printCollections();
+        removeLastPropertyCollectionNameInput.value = '';
+    }
+    clearAllButton.onclick = function(){
+        collections[clearAllCollectionNameInput.value].clearAll();
+        printCollections();
+        clearAllCollectionNameInput.value = '';
+    }
+    //////////////////////
+    printCollections();
 })()
+//{name:"Ban",value:2}
